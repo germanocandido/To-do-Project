9ce9ce0e00e6f7fc5e9e2dd56682d29becb93a40
germanocandido <germanocandido@msn.com>
2022-10-06 07:42:35 -0300

    feat: criação do formulário

--- src/App.js
+++ src/App.js
@@ -11,16 +11,50 @@ function App() {
   const [todos, setTodos] = useState([])
   const [loading, setLoading] = useState(false)
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title);
+
+    setTitle("");
+
+    console.log("Submit");
+    
+  }
+
   return (
     <div className="App">
       <div className="todo-header">
         <h1>React Todo</h1>
       </div>
       <div className="form-todo">
-        <p>Formulário</p>
+        <h2>Insira a sua próxima tarefa:</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor='title'>O que você vai fazer?</label>
+            <input type="text" 
+                    name='title' 
+                    placeholder="Título da tarefa" 
+                    onChange={(e)=> setTitle(e.target.value)}
+                    value={title || ""}
+                    required
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor='time'>Duração:</label>
+            <input type="text" 
+                    name='time' 
+                    placeholder="Tempo estimado (emhoras)" 
+                    onChange={(e)=> setTime(e.target.value)}
+                    value={time || ""}
+                    required
+            />
+          </div>
+          <input type="submit" value="Criar Tarefa" />
+        </form>
       </div>
       <div className='list-todo'>
-        <p>Lista</p>
+        <h2>Lista de tarefas:</h2>
+        {todos.length === 0 && <p>Não há tarefas!</p>}
       </div>
     </div>
   );
